9a2e16a8267b16933bdc7383fd3c0889004e424a
Foneya <gpleyyt@gmail.com>
2025-09-30 13:34:17 +0300

    Refactor
    Refactor and small fix

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ app.use(express.json());
 
 let messages = [];
 
-app.get('/api/messages', (req,res) => {
-    res.json(message);
+app.get('/api/messages', (req, res) => {
+    res.json(messages);
 });
 
-app.post('/api/messages',(req,res)=>{
-    const {text,senderName } = req.body;
-    if(!text || !senderName){
-        return res.status(400).json({ error:'Поля text и senderName обязательны'});
+app.post('/api/messages', (req, res) => {
+    const { text, senderName } = req.body;
+    if (!text || !senderName) {
+        return res.status(400).json({ error: 'Поля text и senderName обязательны' });
     }
     const newMessage = {
         id: Date.now().toString(),
@@ -27,6 +27,6 @@ app.post('/api/messages',(req,res)=>{
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅`);
+app.listen(PORT, '0.0.0.0', () => { 
+  console.log(`✅ Сервер запущен на порту ${PORT}`);
 });
